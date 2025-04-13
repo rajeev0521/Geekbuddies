4c6d0fce28e62f17c1b8d7d8570cb97c259831c3
rajeev0521 <rajeev.382761@gmail.com>
2025-04-13 10:55:13 +0530

    created Splash Screen

---
+++ app/src/main/java/com/rajeev0521/studenthub/splashActivity.kt
@@ -0,0 +1,44 @@
+package com.rajeev0521.studenthub
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.AlphaAnimation
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class splashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        //Reference to the logo image view
+        val logoImage = findViewById<ImageView>(R.id.logoImage)
+
+        //Ease in fade animation
+        val fadeIn = AlphaAnimation(0f, 1f).apply {
+            duration = 2000
+            interpolator = AccelerateInterpolator()
+            fillAfter = true
+        }
+
+        logoImage.startAnimation(fadeIn)
+
+        // Creating Intent to move to main screen
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        },2500)
+    }
+}
